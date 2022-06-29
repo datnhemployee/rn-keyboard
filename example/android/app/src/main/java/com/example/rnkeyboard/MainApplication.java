dcc0e981af87daa7d59dfa453b638fe01cf0cd1b
huy.le3 <huy.le3@mservice.com.vn>
2022-06-29 22:26:55 +0700

    fix babel error

--- example/android/app/src/main/java/com/example/rnkeyboard/MainApplication.java
+++ example/android/app/src/main/java/com/example/rnkeyboard/MainApplication.java
@@ -27,7 +27,7 @@ public class MainApplication extends Application implements ReactApplication {
           List<ReactPackage> packages = new PackageList(this).getPackages();
           // Packages that cannot be autolinked yet can be added manually here, for RnKeyboardExample:
           // packages.add(new MyReactNativePackage());
-          packages.add(new RnKeyboardPackage());
+//          packages.add(new RnKeyboardPackage());
           return packages;
         }
 
